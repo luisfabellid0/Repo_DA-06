3ae870dc8f5d3790376dea6b6aa1586ba0cbe257
luisfabellid0 <132019717+luisfabellid0@users.noreply.github.com>
2024-10-04 09:21:02 -0500

    Titulo

---
+++ Laboratorio 6/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AnimalList from './components/AnimalList/AnimalList';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Encuentra a tu Compañero Perfecto: ¡Adopta un Animalito!</h1>
+      <AnimalList />
+    </div>
+  );
+};
+
+export default App;
