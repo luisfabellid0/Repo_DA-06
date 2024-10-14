ec7a2209ecce09913b2118773096b319edd7a062
luisfabellid0 <132019717+luisfabellid0@users.noreply.github.com>
2024-10-13 23:48:26 -0500

    parte de contactar a la pagina

---
+++ Laboratorio 7/pages/Contact.js
@@ -0,0 +1,12 @@
+import '../styles/Contact.css'; 
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h1>Contáctanos</h1>
+      <p>Puedes contactarnos en: contacto@dassostore.com</p>
+    </div>
+  );
+};
+
+export default Contact;
