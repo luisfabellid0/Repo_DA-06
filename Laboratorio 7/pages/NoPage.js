45562fa067e9d7805a67b0e161287a12daa6fbc6
luisfabellid0 <132019717+luisfabellid0@users.noreply.github.com>
2024-10-13 23:49:46 -0500

    Error

---
+++ Laboratorio 7/pages/NoPage.js
@@ -0,0 +1,10 @@
+const NoPage = () => {
+  return (
+    <div className="container">
+      <h1>404 - Página no encontrada</h1>
+      <p>Lo sentimos, la página que buscas no existe.</p>
+    </div>
+  );
+};
+
+export default NoPage;
