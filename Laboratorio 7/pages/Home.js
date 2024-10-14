8ff23ec2633c8eff8e1f7d4562b3d95b379e8d08
luisfabellid0 <132019717+luisfabellid0@users.noreply.github.com>
2024-10-13 23:49:06 -0500

    Create Home.js

---
+++ Laboratorio 7/pages/Home.js
@@ -0,0 +1,12 @@
+import '../styles/Home.css'; 
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1>Bienvenidos a Dassostore!</h1>
+      <p>Encuentra los mejores periféricos para gamers.</p>
+    </div>
+  );
+};
+
+export default Home;
